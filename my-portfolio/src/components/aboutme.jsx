ef796d3c89d6e227c13aa12eb41b530a84ab30ad
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-04-29 18:44:38 +0200

    Added aboutme.jsx
    Added aboutme component

---
+++ my-portfolio/src/components/aboutme.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './aboutme.css'; // Import CSS file for styling
+
+const AboutMe = () => {
+    return (
+        <section className="about-me">
+            <div className="about-me-content">
+                <h2>About Me</h2>
+                <p>Hello, my name is Philip Älgebrink. I'm currently studying Front-End Developer.<br />
+                    I have a burning interest for making something from nothing, which is why I chose this path.<br />
+                    I've also studied 1 year of Gamemaker where I worked in Unity and C#.<br />
+                    I am right now learning about React.js which is what this portfolio is made in!
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMe;
