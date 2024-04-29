6861b90ef44aa70e45c114ccae61e13b932f785c
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-04-29 19:11:15 +0200

    Changed githubrepos
    Added custom descriptions for each repo
    Added container
    Removed margin from h2 (caused gap between aboutme and githubrepos)
    Added padding to container

--- my-portfolio/src/components/githubrepos.jsx
+++ my-portfolio/src/components/githubrepos.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './githubrepos.css';
 
+// Repo additional descriptions
+const repoDescriptions = {
+    'grupp-1-react': 'A website designed for mobile device that handles your creditcards. (School project)',
+    'Philip_FE23_flexboxMania': 'A website showcasing my flexbox skills (School project)',
+    'Philip_FE23_individuell-examination': 'A website showcasing I know how to use grid and flexbox. (School project)',
+    'Solaris-individuell-examination': 'A website showcasing I know how to use javascript and API to fetch information of our solarsystem. (School project)'
+};
+
 const GitHubRepos = ({ username }) => {
     const [repos, setRepos] = useState([]);
 
@@ -19,13 +27,15 @@ const GitHubRepos = ({ username }) => {
     }, [username]);
 
     return (
-        <div>
+        <div className='repo-container'>
             <h2>My GitHub Repositories</h2>
             <ul className="repo-list">
                 {repos.map(repo => (
                     <li key={repo.id} className="repo-item">
                         <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className="repo-name">{repo.name}</a>
-                        <p className="repo-description">{repo.description}</p>
+                        {repoDescriptions[repo.name] && (
+                            <p className="repo-description">{repoDescriptions[repo.name]}</p>
+                        )}
                     </li>
                 ))}
             </ul>
