2ea99cf2f0da778238911d9ea8878d9767e6e9f5
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-04-29 18:43:23 +0200

    Updated app.jsx

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header';
+import AboutMe from './components/aboutme';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <AboutMe />
+        </div>
+    );
+}
+
+export default App;
