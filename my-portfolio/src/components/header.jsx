0a4c8540bdb7ae55bf450b33dc51eb46ba17a759
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-05-06 18:37:18 +0200

    Added styling to header
    Now correct route is lit up (To easier know where you are)

--- my-portfolio/src/components/header.jsx
+++ my-portfolio/src/components/header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
         <header className="header">
             <h1 className="header-title">Philip Älgebrink</h1>
-            <Link to="/" className="header-link">About Me</Link>
-            <Link to="/github" className="header-link">GitHub Repos</Link>
-            <Link to="/games" className="header-link">Games</Link>
-            <Link to="/contact" className="header-link">Contact</Link>
+            <NavLink to="/" className="header-link" activeclassname="active">About Me</NavLink>
+            <NavLink to="/github" className="header-link" activeclassname="active">GitHub Repos</NavLink>
+            <NavLink to="/games" className="header-link" activeclassname="active">Games</NavLink>
+            <NavLink to="/contact" className="header-link" activeclassname="active">Contact</NavLink>
         </header>
     );
 };
