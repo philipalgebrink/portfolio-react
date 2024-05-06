c53f59193c02bd5c59638038d136501ba4cc398f
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-05-06 15:25:45 +0200

    Configured routing
    Added routing to each view (About me, github repos, games)
    Added way to fetch githubUsername in app.jsx

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import AboutMe from './components/aboutme';
 import GitHubRepos from './components/githubrepos';
@@ -6,19 +7,21 @@ import GameShowcase from './components/gameshowcase';
 import Footer from './components/footer';
 
 function App() {
+    // Pass the github username this way for it to work with routing
+    const githubUsername = 'philipalgebrink';
+
     return (
-        <div className="App">
-            <Header />
-            <AboutMe />
-            <GitHubRepos username="philipalgebrink" />
-            <GameShowcase
-                title="Don't worry child"
-                description="A game I made during my gamemaker course."
-                imageUrl="https://img.itch.zone/aW1nLzkyNTkzMzIucG5n/347x500/iOeSTy.png"
-                gameLink="https://cyranolua.itch.io/dont-worry-child"
-            />
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<AboutMe />} />
+                    <Route path="/github" element={<GitHubRepos username={githubUsername} />} />
+                    <Route path="/games" element={<GameShowcase />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
