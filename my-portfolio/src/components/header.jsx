bf619305bf2991e0fae00f21e87b87b84fedff5c
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-05-06 15:39:31 +0200

    Added styling to header links

--- my-portfolio/src/components/header.jsx
+++ my-portfolio/src/components/header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
     return (
         <header className="header">
             <h1 className="header-title">Philip Älgebrink</h1>
+            <Link to="/" className="header-link">About Me</Link>
+            <Link to="/github" className="header-link">GitHub Repos</Link>
+            <Link to="/games" className="header-link">Games</Link>
+            <Link to="/contact" className="header-link">Contact</Link>
         </header>
     );
 };
