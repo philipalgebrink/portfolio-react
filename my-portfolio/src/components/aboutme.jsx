71c24f098738d35c457b35f90a86be0f010e4dc3
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-05-10 12:53:09 +0200

    Updated styling
    Made all pages have same layout

--- my-portfolio/src/components/aboutme.jsx
+++ my-portfolio/src/components/aboutme.jsx
@@ -6,11 +6,15 @@ const AboutMe = () => {
         <section className="about-me">
             <div className="about-me-content">
                 <h2>About Me</h2>
-                <p>Hello, my name is Philip Älgebrink. I'm currently studying Front-End Developer.<br />
-                    I have a burning interest for making something from nothing, which is why I chose this path.<br />
-                    I've also studied 1 year of Gamemaker where I worked in Unity and C#.<br />
-                    Right now I'm learning about React.js which is what I use for this portfolio!
-                </p>
+                <div className="about-me-item">
+                    <h3 className="about-me-name">Philip Älgebrink</h3>
+                    <p className="about-me-description">
+                        Hello, my name is Philip Älgebrink. I'm currently studying Front-End Developer.<br />
+                        I have a burning interest for making something from nothing, which is why I chose this path.<br />
+                        I've also studied 1 year of Gamemaker where I worked in Unity and C#.<br />
+                        Right now I'm learning about React.js which is what I use for this portfolio!
+                    </p>
+                </div>
             </div>
         </section>
     );
