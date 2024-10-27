858410070e887ec9b1875052883d8c33ca4dfa9d
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-10-27 11:18:12 +0100

    Updated App.jsx
    * Use HashRouter instead of BrowserRouter

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,49 +1,55 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import Header from './components/header';
-import AboutMe from './components/aboutme';
-import GitHubRepos from './components/githubrepos';
-import GameShowcase from './components/gameshowcase';
-import Contact from './components/contactme';
-import Footer from './components/footer';
-import { fetchRepositories } from './actions/repositoryActions';
+import React, { useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import Header from "./components/header";
+import AboutMe from "./components/aboutme";
+import GitHubRepos from "./components/githubrepos";
+import GameShowcase from "./components/gameshowcase";
+import Contact from "./components/contactme";
+import Footer from "./components/footer";
+import { fetchRepositories } from "./actions/repositoryActions";
 
 function App() {
-    // Pass the github username this way for it to work with routing
-    const githubUsername = 'philipalgebrink';
+  // Pass the github username this way for it to work with routing
+  const githubUsername = "philipalgebrink";
 
-    useEffect(() => {
-        // Fetch repositories when the component mounts
-        store.dispatch(fetchRepositories(githubUsername));
-    }, []);
+  useEffect(() => {
+    // Fetch repositories when the component mounts
+    store.dispatch(fetchRepositories(githubUsername));
+  }, []);
 
-    return (
-        <Provider store={store}>
-            <Router>
-                <div className="App">
-                    <Header />
-                    <main>
-                        <Routes>
-                            <Route path="/" element={<AboutMe />} />
-                            <Route path="/github" element={<GitHubRepos username={githubUsername} />} />
-                            <Route path="/games" element={
-                                <GameShowcase
-                                    title="Don't worry child"
-                                    description="A game I made during my gamemaker course."
-                                    imageUrl="https://img.itch.zone/aW1nLzkyNTkzMzIucG5n/347x500/iOeSTy.png"
-                                    gameLink="https://cyranolua.itch.io/dont-worry-child"
-                                />
-                            } />
-                            <Route path="/contact" element={<Contact />} />
-                        </Routes>
-                    </main>
-                    <Footer />
-                </div>
-            </Router>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<AboutMe />} />
+              <Route
+                path="/github"
+                element={<GitHubRepos username={githubUsername} />}
+              />
+              <Route
+                path="/games"
+                element={
+                  <GameShowcase
+                    title="Don't worry child"
+                    description="A game I made during my gamemaker course."
+                    imageUrl="https://img.itch.zone/aW1nLzkyNTkzMzIucG5n/347x500/iOeSTy.png"
+                    gameLink="https://cyranolua.itch.io/dont-worry-child"
+                  />
+                }
+              />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
